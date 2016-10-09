85fb44ff6f1a9ef60740b55ef604003e537c556e
Denys Yablonskyy <mrvayzard@ukr.net>
2016-10-09 16:10:02 +0300

    Update Ex2.cs

--- Ex2.cs
+++ Ex2.cs
@@ -1,54 +1,59 @@
-﻿/* Creating by Yablonskyy Denys <mrvayzard@ukr.net> */
+/* Creating by Yablonskyy Denys <mrvayzard@ukr.net> */
 
 using System;
 using System.Collections.Generic;
 
-class Ex2
+namespace Ex2
 {
-    static List<int> FindCircularPrimeNumbers(int limit)    //finding circular prime numbers
+    class Program
     {
-        bool flag = false;
-        List<int> prime = new List<int>(); //list of circular prime numbers
-        prime.Add(2);
-        for (int i = 3; i < limit; i++)
+        static List<int> FindCircularPrimeNumbers(int limit)    //finding circular prime numbers
         {
-            string number = i.ToString();
-            for (int j = 0; j < number.Length; j++)
+            bool flag = false;  //check is cicular prime or 
+            List<int> prime = new List<int>(); //list of circular prime numbers
+            prime.Add(2);
+            for (int i = 3; i < limit; i++)
             {
-                int num = Convert.ToInt32(number);
-                for (int k = 0; k < prime.Count; k++)
+                string number = i.ToString();
+                for (int j = 0; j < number.Length; j++)
                 {
-                    if (num % prime[k] == 0 && num != prime[k]) //if number not circular prime
+                    int num = Convert.ToInt32(number);
+                    for (int k = 0; k < prime.Count; k++)
                     {
-                        flag = true;
-                        break;
+                        if (num % prime[k] == 0 && num != prime[k]) //if number not circular prime
+                        {
+                            flag = true;
+                            break;
+                        }
                     }
+                    if (flag == true)
+                        break;
+                    //next 3 line is number discharge shift (to left)
+                    char temp = number[0];
+                    number = number.Remove(0, 1);
+                    number += temp;
                 }
-                //next 3 line is number discharge shift (to left)
-                char temp = number[0];
-                number = number.Remove(0, 1);
-                number += temp;
+                if (flag == false)  //if number is circular prime
+                    prime.Add(i);
+                flag = false;
             }
-            if (flag == false)  //if number is circular prime
-                prime.Add(i);
-            flag = false;
+            return prime;   //return list of circular prime numbers
         }
-        return prime;   //return list of circular prime numbers
-    }
 
-    static void Main(string[] args)
-    {
-        int limit = 1000000;    //max number value
-        List <int> circularPrimeNumbers = FindCircularPrimeNumbers(limit);  //list of circular prime numbers
-        Console.WriteLine("Count of cycles prime numbers: " + circularPrimeNumbers.Count);  //output count ofcircular prime numbers to console
+        static void Main(string[] args)
+        {
+            int limit = 1000000;    //max number value
+            List<int> circularPrimeNumbers = FindCircularPrimeNumbers(limit);  //list of circular prime numbers
+            Console.WriteLine("Count of cycles prime numbers: " + circularPrimeNumbers.Count);  //output count ofcircular prime numbers to console
 
-        /*Uncomment to output cycles prime numbers*/
+            /*Uncomment to output cycles prime numbers*/
 
-        //foreach (var item in cyclesPrimeNumbers)
-        //{
-        //    Console.Write(item + " ");
-        //}
+            //foreach (var item in cyclesPrimeNumbers)
+            //{
+            //    Console.Write(item + " ");
+            //}
 
-        Console.Read();
+            Console.Read();
+        }
     }
 }
